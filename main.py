df223ff10e7b133a0a11ef83bb431eef82f281fa
Pranav Pardeshi <im.pranav.pardeshi@gmail.com>
2026-07-07 14:11:33 +0530

    test: add test for rate limit algorithm

--- main.py
+++ main.py
@@ -35,19 +35,19 @@ def ping(limit = Depends(rate_limit)):
     
 
 if __name__ == "__main__":
-    # def test():
-    #     bucket = TokenBucket(capacity=5, refill_rate=1)
+    def test():
+        bucket = TokenBucket(capacity=5, refill_rate=1)
 
-    #     for i in range(5):
-    #         result = bucket.consume()
-    #         print(f"Request {i+1}: {'allowed' if result else 'BLOCKED'} (tokens left: {bucket.tokens:.2f})")
+        for i in range(5):
+            result = bucket.consume()
+            print(f"Request {i+1}: {'allowed' if result else 'BLOCKED'} (tokens left: {bucket.tokens:.2f})")
 
-    #     print("--- waiting 3 seconds ---")
-    #     time.sleep(3)
+        print("--- waiting 3 seconds ---")
+        time.sleep(3)
 
-    #     for i in range(5, 9):
-    #         result = bucket.consume()
-    #         print(f"Request {i+1}: {'allowed' if result else 'BLOCKED'} (tokens left: {bucket.tokens:.2f})")
+        for i in range(5, 9):
+            result = bucket.consume()
+            print(f"Request {i+1}: {'allowed' if result else 'BLOCKED'} (tokens left: {bucket.tokens:.2f})")
 
-    # test()
-    pass
+    test()
+    
